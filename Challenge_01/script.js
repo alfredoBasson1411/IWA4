b39da6c87ccdf248d112b96023f06f96bd4f6233
alfredoBasson1411 <alfredojemainebasson14@gmail.com>
2024-01-31 18:35:47 +0200

    added comments in for better understanding

--- Challenge_01/script.js
+++ Challenge_01/script.js
@@ -1,35 +1,43 @@
-//changed"const" to "let"
+// changed "const" to "let"
 let date = 2050;
 let status = 'student';
 let count = 0;
 
-/**added equality operator
+/** added equality operator
  * removed "let" keyword at "count" and added "+=" operator
-*/
+ */
 if (date === 2050) {
   console.log("January", 'New Year’s Day');
   console.log("March", 'Human Rights Day');
-  date = 'April';
-  console.log(date, 'Family Day');
-  console.log(date, 'Freedom Day');
+  // changed date = 'April'; to let aprilDate = 'April'; for clarity
+  let aprilDate = 'April';
+  console.log(aprilDate, 'Family Day');
+  console.log(aprilDate, 'Freedom Day');
+  // changed let count = count + 4; to count += 4; for shorthand
   count += 4;
+
   if (status === "student") {
     console.log('June', 'Youth Day');
+    // changed let count = count + 1; to count += 1; for shorthand
     count += 1;
   }
 
   console.log('August', "Women's Day");
   console.log('September', 'Heritage Day');
-  date = 'December';
-  console.log(date, 'Day of Reconciliation');
+  // changed date = 'December'; to let decemberDate = 'December'; for clarity
+  let decemberDate = 'December';
+  console.log(decemberDate, 'Day of Reconciliation');
+  // changed let count = count + 3; to count += 3; for shorthand
   count += 3;
 
   if (status === "parent") {
-    console.log(date, 'Christmas Day');
+    console.log(decemberDate, 'Christmas Day');
+    // changed let count = count + 1; to count += 1; for shorthand
     count += 1;
   }
 
-  console.log(date, 'Day of Goodwill');
+  console.log(decemberDate, 'Day of Goodwill');
+  // changed let count = count + 1; to count += 1; for shorthand
   count += 1;
 }
 
