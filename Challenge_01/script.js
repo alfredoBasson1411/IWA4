72986288bb3530996024e0d0bd019cc794188656
alfredoBasson1411 <alfredojemainebasson14@gmail.com>
2024-01-31 18:58:39 +0200

    changes the strike through status to 'status1' & added extra 'let'declaration

--- Challenge_01/script.js
+++ Challenge_01/script.js
@@ -1,8 +1,8 @@
 // changed "const" to "let"
 let date = 2050;
-let status = 'student';
+let status1 = 'student';
 let count = 0;
-
+let aprilDate= null
 /** added equality operator
  * removed "let" keyword at "count" and added "+=" operator
  */
@@ -10,13 +10,13 @@ if (date === 2050) {
   console.log("January", 'New Year’s Day');
   console.log("March", 'Human Rights Day');
   // changed date = 'April'; to let aprilDate = 'April'; for clarity
-  let aprilDate = 'April';
+  aprilDate = 'April';
   console.log(aprilDate, 'Family Day');
   console.log(aprilDate, 'Freedom Day');
   // changed let count = count + 4; to count += 4; for shorthand
   count += 4;
 
-  if (status === "student") {
+  if (status1 === "student") {
     console.log('June', 'Youth Day');
     // changed let count = count + 1; to count += 1; for shorthand
     count += 1;
@@ -25,12 +25,12 @@ if (date === 2050) {
   console.log('August', "Women's Day");
   console.log('September', 'Heritage Day');
   // changed date = 'December'; to let decemberDate = 'December'; for clarity
-  let decemberDate = 'December';
+  decemberDate = 'December';
   console.log(decemberDate, 'Day of Reconciliation');
   // changed let count = count + 3; to count += 3; for shorthand
   count += 3;
 
-  if (status === "parent") {
+  if (status1 === "parent") {
     console.log(decemberDate, 'Christmas Day');
     // changed let count = count + 1; to count += 1; for shorthand
     count += 1;
